9e39f2cbb1aca60ccfa2ec7e2c18909089d98cb9
serrie <mohammadzidane058@gmail.com>
2025-06-29 14:00:30 +0700

    revert: login and register endpoint

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,5 @@ const userController = require('../controllers/userController');
 const auth = require('../middlewares/authMiddleware');
 
 router.get('/', userController.getAllUsers);
-router.post('/register', userController.register);
-router.post('/login', userController.login);
 
 module.exports = router;
