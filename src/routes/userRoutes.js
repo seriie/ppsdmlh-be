b3cf772ea4fbeb8d9b0c19098e1ff471436eed03
serrie <mohammadzidane058@gmail.com>
2025-06-29 13:44:36 +0700

    feat: add login and register endpoint

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers } = require('../controllers/userController');
+const userController = require('../controllers/userController');
 const auth = require('../middlewares/authMiddleware');
 
-router.get('/', getAllUsers);
+router.get('/', userController.getAllUsers);
+router.post('/register', userController.register);
+router.post('/login', userController.login);
 
 module.exports = router;
